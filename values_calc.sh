fa53f89970652f3be67959679f4a710745b67162
dimitrisdol <jimmdologlou@gmail.com>
2023-05-22 12:49:20 +0300

    Removed logs

--- values_calc.sh
+++ values_calc.sh
@@ -7,8 +7,6 @@ avg_cpu=$(echo "$cpu_values" | awk '{sum+=$1; count++} END {print sum/count}')
 echo "Max CPU usage: $max_cpu%"
 echo "Average CPU usage: $avg_cpu%"
 
-#!/bin/bash
-
 mem_values=$(awk -F'[:%]' '/MEM/ {print $4}' monitor.log)
 max_mem=$(echo "$mem_values" | sort -nr | head -n 1)
 avg_mem=$(echo "$mem_values" | awk '{sum+=$1; count++} END {print sum/count}')
